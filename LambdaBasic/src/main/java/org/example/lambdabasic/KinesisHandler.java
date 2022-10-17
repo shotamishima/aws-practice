e7b28ea0c38d920ee5dc45a1b9111090e35b1f7a
shotamishima <sm4mma@gmail.com>
2022-10-17 10:06:50 +0900

    Enable dynamo handler

--- LambdaBasic/src/main/java/org/example/lambdabasic/KinesisHandler.java
+++ LambdaBasic/src/main/java/org/example/lambdabasic/KinesisHandler.java
@@ -11,7 +11,6 @@ import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class KinesisHandler implements RequestHandler<KinesisEvent, String> {
@@ -22,33 +21,36 @@ public class KinesisHandler implements RequestHandler<KinesisEvent, String> {
         String response = "200 OK";
         String table_name = "Ticker";
 
-        HashMap<String, AttributeValue> item_values = new HashMap<String, AttributeValue>();
-
-        item_values.put("id", new AttributeValue("1"));
-        item_values.put("tickerSymbol", new AttributeValue("MSFT"));
-        item_values.put("price", new AttributeValue("100"));
-
         final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.standard().build();
-
-        try {
-            ddb.putItem(table_name, item_values);
-        } catch (ResourceNotFoundException e) {
-            System.err.format("Error: The table \"%s\" can't be found.\n", table_name);
-            System.err.println("Be sure that it exists and that you've typed its name correctly.");
-            System.exit(1);
-        } catch (AmazonServiceException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
-
         ObjectMapper JSON = new ObjectMapper();
 
         for (KinesisEventRecord record : kinesisEvent.getRecords()) {
-            // System.out.println(new String(record.getKinesis().getData().array()));
             try {
+                // Deserialize record got from Kinesis
                 StockTrade trade =
                         JSON.readValue(record.getKinesis().getData().array(), StockTrade.class);
                 System.out.println(trade.toString());
+
+                // DynamoDB
+                // Field is applicable only string.
+                // Want to change field type for each value's type
+                HashMap<String, AttributeValue> item_values = new HashMap<String, AttributeValue>();
+                item_values.put("id", new AttributeValue(String.valueOf(trade.getId())));
+                item_values.put("tickerSymbol", new AttributeValue(trade.getTickerSymbol()));
+                item_values.put("price", new AttributeValue(String.valueOf(trade.getPrice())));
+
+                try {
+                    System.out.println("Write into DynamoDB...")
+                    ddb.putItem(table_name, item_values);
+                } catch (ResourceNotFoundException e) {
+                    System.err.format("Error: The table \"%s\" can't be found.\n", table_name);
+                    System.err.println(
+                            "Be sure that it exists and that you've typed its name correctly.");
+                    System.exit(1);
+                } catch (AmazonServiceException e) {
+                    System.err.println(e.getMessage());
+                    System.exit(1);
+                }
             } catch (IOException e) {
                 System.out.println(e);
                 return null;
